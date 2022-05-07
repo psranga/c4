4925ab75a26f42186bfe19f7f54965b8d580102b
Ranga Sankaralingam <ranga@utexas.edu>
2022-05-07 14:31:00 -0700

    Fixes int needing to be the same wide as int* for armv7l also (tested on Raspberry Pi).

--- c4.c
+++ c4.c
@@ -11,7 +11,7 @@
 #include <memory.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define int long long
+#define int intptr_t
 
 char *p, *lp, // current position in source code
      *data;   // data/bss pointer
